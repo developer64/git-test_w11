a38b5ff632fd61096fdb797f0a84ef3f67e4b42f
Developer64 <simlim2013@yandex.ru>
2024-05-18 10:50:37 +0300

    Basic JS

--- js/main.js
+++ js/main.js
@@ -387,76 +387,99 @@
 
 // Example#1
 
-class People {
-    /**
-     * 
-     * @param {String} fio по формату "Фамилия Имя Отчество"
-     * @param {String} birthday день рождения по формату "24.11.1994" 
-     * @param {String} numbers "номер", если больше 2 номеров, то по формату "номер1, номер2"
-     * @param {Number} room комната
-     */
-    constructor(fio,birthday,numbers="",room="") {
-        let name = fio.toLowerCase().split(" "); //Перевод в нижний регистр и разделяем
-        this.name = {};
-        this.name.f = name[0][0].toUpperCase() + name[0].slice(1)
-        this.name.i = name[1][0].toUpperCase() + name[1].slice(1)
-        this.name.o = name[2][0].toUpperCase() + name[2].slice(1)
+// class People {
+//     /**
+//      * 
+//      * @param {String} fio по формату "Фамилия Имя Отчество"
+//      * @param {String} birthday день рождения по формату "24.11.1994" 
+//      * @param {String} numbers "номер", если больше 2 номеров, то по формату "номер1, номер2"
+//      * @param {Number} room комната
+//      */
+//     constructor(fio,birthday,numbers="",room="") {
+//         let name = fio.toLowerCase().split(" "); //Перевод в нижний регистр и разделяем
+//         this.name = {};
+//         this.name.f = name[0][0].toUpperCase() + name[0].slice(1)
+//         this.name.i = name[1][0].toUpperCase() + name[1].slice(1)
+//         this.name.o = name[2][0].toUpperCase() + name[2].slice(1)
 
-        let date = birthday.split(".");
-        this.date = {};
-        this.date.d = +date[0]
-        this.date.m = +date[1]
-        this.date.y = +date[2]
+//         let date = birthday.split(".");
+//         this.date = {};
+//         this.date.d = +date[0]
+//         this.date.m = +date[1]
+//         this.date.y = +date[2]
 
-        this.numbers = numbers.split(",");
-        this.room = +room
-    }
-    static month = ["январь", "феврль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
-    /**
-     * Ввести ФИО
-     * @return (String) Строка формата "Фамилия Имя Отчество"
-     */
-    getFio() {
-        return `${this.name.f} ${this.name.i} ${this.name.o}`
-    }
-    /**
-     * Ввести день рождения
-     * @return (String) Строка формата "24 июля 1997"
-     */
-    getBirthday() {
-        return `${this.date.d} ${People.month[this.date.m-1]} ${this.date.y}`
-    }
-    /**
-     * Вывести Фамилия_Имя_Оотчество
-     * @param {String} extention расширение, например "jpg" или "png"
-     * @return {String} для названия изображения
-     */
-    getImgSrc(extention) {
-        return `${this.name.f}_${this.name.i}_${this.name.o}.${extention}`
-    }
-    /**
-     * Вывести все номера телефонов или только первый номер
-     * @param {Boolean} allNumbers  если true, то все номера, если false только первый номер
-     * return {String} Строка формата:"89006660099, 89009996699"
-     */
-    getNumbersList(allNumbers) {
-        if(this.numbers.length == 0 || this.numbers[0].length == 0)
-            return undefined
-        if(allNumbers) {
-            return this.numbers.join(",")
-        }else {
-            return this.numbers[0];
-        }
-    }
-}
-let people1 = new People("Мянкин Михаил Иванович", "24.11.1994", "9984, 8877, 5423", 542)
-let people2 = new People("Иванов Иван Иванович", "20.12.1984", "",)
-console.log(people1.getFio())
-console.log(people2.getFio())
-console.log(people1.getBirthday())
-console.log(people2.getBirthday())
+//         this.numbers = numbers.split(",");
+//         this.room = +room
+//     }
+//     static month = ["январь", "феврль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
+//     /**
+//      * Ввести ФИО
+//      * @return (String) Строка формата "Фамилия Имя Отчество"
+//      */
+//     getFio() {
+//         return `${this.name.f} ${this.name.i} ${this.name.o}`
+//     }
+//     /**
+//      * Ввести день рождения
+//      * @return (String) Строка формата "24 июля 1997"
+//      */
+//     getBirthday() {
+//         return `${this.date.d} ${People.month[this.date.m-1]} ${this.date.y}`
+//     }
+//     /**
+//      * Вывести Фамилия_Имя_Оотчество
+//      * @param {String} extention расширение, например "jpg" или "png"
+//      * @return {String} для названия изображения
+//      */
+//     getImgSrc(extention) {
+//         return `${this.name.f}_${this.name.i}_${this.name.o}.${extention}`
+//     }
+//     /**
+//      * Вывести все номера телефонов или только первый номер
+//      * @param {Boolean} allNumbers  если true, то все номера, если false только первый номер
+//      * return {String} Строка формата:"89006660099, 89009996699"
+//      */
+//     getNumbersList(allNumbers) {
+//         if(this.numbers.length == 0 || this.numbers[0].length == 0)
+//             return undefined
+//         if(allNumbers) {
+//             return this.numbers.join(",")
+//         }else {
+//             return this.numbers[0];
+//         }
+//     }
+// }
+// let people1 = new People("Мянкин Михаил Иванович", "24.11.1994", "9984, 8877, 5423", 542)
+// let people2 = new People("Иванов Иван Иванович", "20.12.1984", "",)
+// console.log(people1.getFio())
+// console.log(people2.getFio())
+// console.log(people1.getBirthday())
+// console.log(people2.getBirthday())
 
-// Lesson#12 Возможности ES6 или ECMAScript 2015, Супер JavaScript, Основы. 
+// Lesson#12 Возможности ES6 или ECMAScript 2015, Супер JavaScript, Основы.   
+
+// console.log(Math.sinh(0))
+// console.log(Math.cosh(0))
+// console.log(Math.tanh(0))
+// console.log(Math.acosh(1))
+// console.log(Math.asinh(0))
+// console.log(Math.atanh(0))
+// console.log(Math.hypot(2,2,1))
+
+// console.log("Str".repeat(3)) //Метод повторения
+
+// let str = "Hello i'm simple String"
+// console.log(str.includes("simple",12))
+// console.log(str.startsWith("Hello"))
+// console.log(str.endsWith("ing"))
+
+// let arr = [1,5,4,7,8,91,1,5] //Метод выявления повторяющих элементов
+// let arr2 = [...new Set(arr)]
+// console.log(arr2)
+
+// #1 Работа с DOM деревом на JavaScript, изменение текста и стилей на JS
+
+
 
 
 
